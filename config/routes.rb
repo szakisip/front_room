830ff31070472de40f13216ed434ab972465c387
szakisip <ssg2012.yamato.s@gmail.com>
2022-01-03 22:10:18 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< Updated upstream
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+=======
+  devise_for :users
+  root to:"talk#index"
+>>>>>>> Stashed changes
 end
